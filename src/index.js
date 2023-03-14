628bf3abd3939ff7b17300120580a8745bf8334a
Kirdandan <elvinaxmedov9r22015@gmail.com>
2023-03-14 10:39:26 +0400

    #

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.scss";
 import App from "./App";
-import { UserContext, UserProvider } from "./context/user.context";
+import { UserProvider } from "./context/user.context";
+import { ProductsProvider } from "./context/products.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
