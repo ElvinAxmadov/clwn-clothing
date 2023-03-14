e458dd4442bb86049b9b8cec5a66a1f9b4945195
Kirdandan <elvinaxmedov9r22015@gmail.com>
2023-03-14 16:06:18 +0400

    #

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -4,13 +4,9 @@ const FormInput = ({ label, ...otherProps }) => {
   return (
     <div className='group'>
       <input className='form-input' {...otherProps} />
-      {label && (
-        <label
-          className={`${otherProps.value.length ? 'shrink' : ''
-            } form-input-label`}
-        >
-          {label}
-        </label>
+      {label && (<label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>
+        {label}
+      </label>
       )}
     </div>
   );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,11 +2,15 @@ import { Fragment, useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import "./navigation.styles.scss";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { UserContext } from "../../context/user.context";
+import { CartContext, CartProvider } from "../../context/cart.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext)
+    const { isCartOpen } = useContext(CartContext)
 
     return (
         <Fragment>
@@ -25,10 +29,10 @@ const Navigation = () => {
                             <Link className='nav-link' to='/auth'>
                                 SIGN IN
                             </Link>
-                        )
-                    }
-
+                        )}
+                    <CartIcon />
                 </div>
+                {isCartOpen && <CartDropdown />}
             </div>
             <Outlet />
         </Fragment>
