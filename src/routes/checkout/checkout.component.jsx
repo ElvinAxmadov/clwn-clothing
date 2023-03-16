eadc6a629e229de47fab0c4e2163b89d3e4bdb70
Kirdandan <elvinaxmedov9r22015@gmail.com>
2023-03-16 17:50:49 +0400

    #

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,9 +1,23 @@
 import "./checkout.styles.scss";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 
 const Checkout = () => {
+    const { cartItems, addItemToCart } = useContext(CartContext)
     return (
         <div>
             <h1>Checkout Page</h1>
+            <div>
+                {cartItems.map((cartItem) => {
+                    const { id, name, quantity } = cartItem
+                    return <div key={id}>
+                        <h2>{name}</h2>
+                        <span>{quantity}</span>
+                        <span>decrement</span>
+                        <span onClick={()=>addItemToCart(cartItem)}>increment</span>
+                    </div>
+                })}
+            </div>
         </div>
     )
 }
