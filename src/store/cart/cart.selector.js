ed6125501996f66432f4e692e3459ec087781399
Kirdandan <elvinaxmedov9r22015@gmail.com>
2023-04-06 11:28:46 +0400

    #

--- src/store/cart/cart.selector.js
+++ src/store/cart/cart.selector.js
@@ -1,14 +1,13 @@
-// import { createSelector } from "reselect";
+import { createSelector } from "reselect";
 
-// const selectCartReducer = (state) => state.cart;
+const selectCartReducer = (state) => state.cart;
 
+const newCartCount = cartItems.reduce(
+  (total, cartItem) => total + cartItem.quantity,
+  0
+);
 
-// const newCartCount = cartItems.reduce(
-//   (total, cartItem) => total + cartItem.quantity,
-//   0
-// );
-
-// const newCartTotal = cartItems.reduce(
-//   (total, cartItem) => total + cartItem.quantity * cartItem.price,
-//   0
-// );
+const newCartTotal = cartItems.reduce(
+  (total, cartItem) => total + cartItem.quantity * cartItem.price,
+  0
+);
