349350f9a7b7c29ed44ad0d8f2a55e8dbe18eb95
Kirdandan <elvinaxmedov9r22015@gmail.com>
2023-04-07 16:33:23 +0400

    #

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { persistor, store } from "./store/store";
+import { store, persistor } from "./store/store";
 import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 import "./index.scss";
